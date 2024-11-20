046a9b8266fba7892cddc042a36f92378f5e7f4c
Bruno Chaves <bruno@chaves.fr>
2024-11-20 14:49:01 +0100

    sort people

--- src/routes/people/+page.server.ts
+++ src/routes/people/+page.server.ts
@@ -4,7 +4,7 @@ import type { PeopleResponse } from '$types/people';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	try {
-		const response = await fetch('https://cms.serg.paris/api/people?populate=*&pagination[pageSize]=1000');
+		const response = await fetch('https://cms.serg.paris/api/people?sort=last_name:asc&pagination[pageSize]=1000&populate=*');
 
 		if (!response.ok) {
 			throw new Error('Failed to fetch data');
