76d4938dcc14ac933bd8aaab667bda8ba4731757
Bruno Chaves <bruno@chaves.fr>
2024-11-20 14:43:14 +0100

    sort events

--- src/routes/events/+page.server.ts
+++ src/routes/events/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	try {
-		const response = await fetch('https://cms.serg.paris/api/events?populate=*');
+		const response = await fetch('https://cms.serg.paris/api/events?sort=date_start:desc&populate=*');
 
 		if (!response.ok) {
 			throw new Error('Failed to fetch data');
