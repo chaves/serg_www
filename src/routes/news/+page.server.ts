68d358440a02b5709162cf2e1013cc4b9ce5fa58
Bruno Chaves <bruno@chaves.fr>
2024-11-20 14:53:13 +0100

    sort news

--- src/routes/news/+page.server.ts
+++ src/routes/news/+page.server.ts
@@ -4,7 +4,7 @@ import { flattenObj } from '$lib/utils';
 
 export const load: PageServerLoad = async ({ fetch }) => {
   try {
-    const response = await fetch('https://cms.serg.paris/api/news?populate=*');
+    const response = await fetch('https://cms.serg.paris/api/news?&sort=createdAt:desc&populate=*');
 
     if (!response.ok) {
       throw new Error('Failed to fetch data');
