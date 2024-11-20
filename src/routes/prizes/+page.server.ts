1324e239637a1bf07a7d09fff744a9e78b12995d
Bruno Chaves <bruno@chaves.fr>
2024-11-20 14:06:34 +0100

    sort prizes

--- src/routes/prizes/+page.server.ts
+++ src/routes/prizes/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
   try {
-    const response = await fetch('https://cms.serg.paris/api/prizes?populate=*');
+    const response = await fetch('https://cms.serg.paris/api/prizes?sort[0]=year:desc&sort[1]=last_name');
 
     if (!response.ok) {
       throw new Error('Failed to fetch data');
