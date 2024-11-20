df8a9f3aa8cff670efcdf79bb961698fc5d09ec7
Bruno Chaves <bruno@chaves.fr>
2024-11-20 14:12:53 +0100

    sort WP

--- src/routes/working-papers/+page.server.ts
+++ src/routes/working-papers/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	try {
-		const response = await fetch('https://cms.serg.paris/api/working-papers?populate=*');
+		const response = await fetch('https://cms.serg.paris/api/working-papers?sort[0]=year:desc&sort[1]=createdAt:desc');
 
 		if (!response.ok) {
 			throw new Error('Failed to fetch data');
