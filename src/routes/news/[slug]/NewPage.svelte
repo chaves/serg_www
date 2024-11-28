496e744fc6a5e85c89e199b2d378994d29b14d87
Bruno Chaves <bruno@chaves.fr>
2024-11-28 12:14:06 +0100

    fix picture 2

--- src/routes/news/[slug]/NewPage.svelte
+++ src/routes/news/[slug]/NewPage.svelte
@@ -1,18 +1,19 @@
 <!-- src/components/NewCard.svelte -->
 <script lang="ts">
-    import type { New } from '$types/news';
-    import AreasTags from '$lib/components/AreasTags.svelte';
-    import DownloadFiles from '$lib/components/DownloadFiles.svelte';
-    import { formatDate } from '$lib/utils';
+	import type { New } from '$types/news';
+	import AreasTags from '$lib/components/AreasTags.svelte';
+	import DownloadFiles from '$lib/components/DownloadFiles.svelte';
+	import { formatDate } from '$lib/utils';
 
-    export let article: New;
-  </script>
+	export let article: New;
+</script>
 
 <h1>{article.title}</h1>
 
 <div>{@html article.description}</div>
-
-<div>{article.picture.url}</div>
+{#if article.picture}
+	<div>{article.picture.url}</div>
+{/if}
 
 <DownloadFiles files={article.attached_files} />
 <AreasTags areas={article.areas} />
