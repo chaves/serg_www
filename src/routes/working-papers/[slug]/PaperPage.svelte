95e9581bb466bb1c6315aeb38b841089cefe1da5
Bruno Chaves <bruno@chaves.fr>
2024-12-02 15:26:13 +0100

    papers

--- src/routes/working-papers/[slug]/PaperPage.svelte
+++ src/routes/working-papers/[slug]/PaperPage.svelte
@@ -1,10 +1,17 @@
 <!-- src/components/PersonCard.svelte -->
 <script lang="ts">
-	export let paper;
+	import type { Paper } from '$types/papers';
+	import AreasTags from '$lib/components/AreasTags.svelte';
+	import DownloadFiles from '$lib/components/DownloadFiles.svelte';
+	export let paper: Paper;
 </script>
 
-{paper.title}
+<AreasTags areas={paper.areas} />
 
+<p class="mt-3">{paper.authors}</p>
+
+{@html paper.abstract}
+
+<DownloadFiles files={paper.files} />
 <style>
-
 </style>
