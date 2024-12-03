efc36b58d34ec97535a6d9df40935276b9ffa069
Bruno Chaves <bruno@chaves.fr>
2024-12-03 08:43:58 +0100

    hal 2

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,8 +1,21 @@
+<script>
+	import logo_hal from '$lib/assets/images/HAL_logotype-rvb_fond-clair_fr.png?enhanced&w=400;200';
+</script>
+
 <svelte:head>
 	<title>Sustainable Economy Research Group (SERG) - Publications</title>
 </svelte:head>
 
 <h1>Publications</h1>
+
+<a href="https://centralesupelec.hal.science/LGI">
+	<enhanced:img
+		src={logo_hal}
+		alt="HAL archives ouvertes"
+		sizes="(min-width:1080px) 400px, (min-width:768px) 200px"
+		class="mx-auto -my-9"
+	/>
+</a>
 
 <div class="space-y-6">
 	<iframe
