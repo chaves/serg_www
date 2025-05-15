e19143ae273f5cb1fc85fab646446995e4610476
Bruno Chaves <bruno@chaves.fr>
2025-05-15 17:56:03 +0200

    add images to prizes

--- src/routes/prizes/+page.server.ts
+++ src/routes/prizes/+page.server.ts
@@ -1,25 +1,27 @@
 // src/routes/people/+page.server.ts
-import type { PrizesResponse } from "../../types/prizes";
+import type { PrizesResponse } from '../../types/prizes';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-  try {
-    const response = await fetch('https://cms.serg.paris/api/prizes?sort[0]=year:desc&sort[1]=last_name');
+	try {
+		const response = await fetch(
+			'https://cms.serg.paris/api/prizes?sort[0]=year:desc&sort[1]=last_name&populate=*&pagination[pageSize]=1000'
+		);
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
+		if (!response.ok) {
+			throw new Error('Failed to fetch data');
+		}
 
-    const data: PrizesResponse = await response.json();
+		const data: PrizesResponse = await response.json();
 
-    return {
-      prizes: data,
-    };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      prizes: null,
-      error: (error as Error).message,
-    };
-  }
+		return {
+			prizes: data
+		};
+	} catch (error) {
+		console.error('Error fetching data:', error);
+		return {
+			prizes: null,
+			error: (error as Error).message
+		};
+	}
 };
--- src/routes/prizes/+page.svelte
+++ src/routes/prizes/+page.svelte
@@ -16,8 +16,15 @@
 <ul class="prize-list bg-white p-5 rounded-lg shadow-md">
 	{#if data.prizes}
 		{#each data.prizes.data as prize}
-			<li class="prize-item mb-2.5 text-lg bg-gray-100 p-1.5 odd:bg-white rounded">
+			<li class="prize-item mb-2.5 text-lg bg-gray-100 p-1.5 odd:bg-white rounded h-20">
 				{prize.year} - <span class="font-semibold">{prize.first_name} {prize.last_name}</span> : {prize.title}
+				{#if prize.picture}
+					<img
+						src={`https://cms.serg.paris${prize.picture.formats.thumbnail.url}`}
+						alt={`${prize.first_name} ${prize.last_name}`}
+						class="rounded-xl h-full float-right ml-4 object-cover"
+					/>
+				{/if}
 			</li>
 		{/each}
 	{:else if data.error}
